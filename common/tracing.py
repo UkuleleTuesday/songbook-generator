3fadf322b2816089656ce6231eea6f5d6d4cfc51
Jeremie Jost <jeremiejost@gmail.com>
2025-07-06 17:39:07 +0100

    feat: integrate Google authentication for OpenTelemetry tracing setup

--- common/tracing.py
+++ common/tracing.py
@@ -1,23 +1,36 @@
 """OpenTelemetry tracing setup for Google Cloud Trace."""
 
 import os
-from opentelemetry import _events as events
-from opentelemetry import _logs as logs
-from opentelemetry import metrics, trace
+from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.resources import SERVICE_INSTANCE_ID, Resource
-from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
-from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk._events import EventLoggerProvider
-from opentelemetry.sdk._logs import LoggerProvider
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 
+import google.auth
+import google.auth.transport.grpc
+import google.auth.transport.requests
+import grpc
+from google.auth.transport.grpc import AuthMetadataPlugin
 
 def setup_tracing():
+    # Retrieve and store Google application-default credentials
+    credentials, project_id = google.auth.default()
+    # Request used to refresh credentials upon expiry
+    request = google.auth.transport.requests.Request()
+
+    # Supply the request and credentials to AuthMetadataPlugin
+    # AuthMeatadataPlugin inserts credentials into each request
+    auth_metadata_plugin = AuthMetadataPlugin(
+        credentials=credentials, request=request
+    )
+
+    # Initialize gRPC channel credentials using the AuthMetadataPlugin
+    channel_creds = grpc.composite_channel_credentials(
+        grpc.ssl_channel_credentials(),
+        grpc.metadata_call_credentials(auth_metadata_plugin),
+    )
+
     resource = Resource.create(
         attributes={
             # Use the PID as the service.instance.id to avoid duplicate timeseries
@@ -28,19 +41,10 @@ def setup_tracing():
 
     # Set up OpenTelemetry Python SDK
     tracer_provider = TracerProvider(resource=resource)
-    tracer_provider.add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
+    tracer_provider.add_span_processor(
+        BatchSpanProcessor(OTLPSpanExporter(credentials=channel_creds))
+    )
     trace.set_tracer_provider(tracer_provider)
-
-    logger_provider = LoggerProvider(resource=resource)
-    logger_provider.add_log_record_processor(BatchLogRecordProcessor(OTLPLogExporter()))
-    logs.set_logger_provider(logger_provider)
-
-    event_logger_provider = EventLoggerProvider(logger_provider)
-    events.set_event_logger_provider(event_logger_provider)
-
-    reader = PeriodicExportingMetricReader(OTLPMetricExporter())
-    meter_provider = MeterProvider(metric_readers=[reader], resource=resource)
-    metrics.set_meter_provider(meter_provider)
 
 
 def get_tracer(name: str):
