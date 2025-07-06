489eb7056fd2009a9c3d971bb1ea646b7c2818b0
Jeremie Jost <jeremiejost@gmail.com>
2025-07-06 17:19:00 +0100

    Trailing space

--- common/test_tracing.py
+++ common/test_tracing.py
@@ -7,7 +7,7 @@ from unittest.mock import patch
 def test_setup_tracing_runs_without_error():
     """Test that setup_tracing can be called without raising exceptions."""
     from tracing import setup_tracing
-    
+
     # Should not raise any exceptions
     setup_tracing()
 
@@ -16,9 +16,9 @@ def test_get_tracer_with_project_id():
     """Test that get_tracer returns a tracer when project ID is available."""
     with patch.dict(os.environ, {"GOOGLE_CLOUD_PROJECT": "test-project"}):
         from tracing import get_tracer
-        
+
         tracer = get_tracer("test-service")
-        
+
         # Should return a tracer with expected methods
         assert hasattr(tracer, "start_span") or hasattr(tracer, "start_as_current_span")
 
@@ -28,9 +28,9 @@ def test_get_tracer_no_project_id():
     with patch.dict(os.environ, {}, clear=True):
         from tracing import get_tracer
         from opentelemetry.trace import NoOpTracer
-        
+
         result = get_tracer("test-service")
-        
+
         # Should return a NoOpTracer instance
         assert isinstance(result, NoOpTracer)
 
@@ -38,8 +38,8 @@ def test_get_tracer_no_project_id():
 def test_get_tracer_returns_callable():
     """Test that get_tracer returns something that looks like a tracer."""
     from tracing import get_tracer
-    
+
     tracer = get_tracer("test-service")
-    
+
     # Should have tracer methods
     assert hasattr(tracer, "start_span") or hasattr(tracer, "start_as_current_span")
