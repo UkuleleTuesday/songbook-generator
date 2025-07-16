f1f692ab7477f72a27a08e8efc0e5547e091cfae
Jeremie Jost <jeremiejost@gmail.com>
2025-07-16 14:29:36 +0100

    fix: Improve log message for full sync condition

--- generator/merger/main.py
+++ generator/merger/main.py
@@ -146,9 +146,7 @@ def _get_last_merge_time(cache_bucket, tracer_span=None) -> Optional[datetime]:
                 tracer_span.set_attribute("last_merge_time", str(last_merge_time))
         return last_merge_time
     except gcp_exceptions.NotFound:
-        print(
-            "No previous merged PDF found. Performing a full sync."
-        )
+        print("No previous merged PDF found. Performing a full sync.")
         if tracer_span:
             tracer_span.set_attribute("last_merge_time", "None")
         return None
